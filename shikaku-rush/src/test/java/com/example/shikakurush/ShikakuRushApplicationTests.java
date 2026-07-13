01c68a20b5c973653a85255f7ae089f9d6722bf7
sugata <k.sugata.sys24@morijyobi.ac.jp>
2026-07-13 14:31:16 +0900

    ディレクトリ構成の構築

---
+++ shikaku-rush/src/test/java/com/example/shikakurush/ShikakuRushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shikakurush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShikakuRushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
